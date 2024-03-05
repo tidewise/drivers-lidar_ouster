74f8eb9a212f351f6c712717cdb159d67b86a49b
Rayssa Soares <rayssa.soares@tidewise.io>
2024-03-05 16:56:20 -0300

    Initial commit

---
+++ src/Dummy.hpp
@@ -0,0 +1,16 @@
+#ifndef _DUMMYPROJECT_DUMMY_HPP_
+#define _DUMMYPROJECT_DUMMY_HPP_
+
+namespace lidar_ouster {
+    class DummyClass {
+    public:
+        /**
+         * Print a welcome to stdout
+         * \return nothing
+         */
+        void welcome();
+    };
+
+} // end namespace lidar_ouster
+
+#endif // _DUMMYPROJECT_DUMMY_HPP_
---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <lidar_ouster/Dummy.hpp>
+
+int main(int argc, char** argv)
+{
+    lidar_ouster::DummyClass dummyClass;
+    dummyClass.welcome();
+
+    return 0;
+}
