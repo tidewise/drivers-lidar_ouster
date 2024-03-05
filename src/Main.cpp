1c485f184f2789c7eafdf9a6704397b8c5677026
Rayssa Soares <rayssa.soares@tidewise.io>
2024-03-05 17:54:59 -0300

    chore: remove dummy files and configure package

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,10 +1,5 @@
 #include <iostream>
-#include <lidar_ouster/Dummy.hpp>
 
-int main(int argc, char** argv)
 {
-    lidar_ouster::DummyClass dummyClass;
-    dummyClass.welcome();
 
-    return 0;
 }
